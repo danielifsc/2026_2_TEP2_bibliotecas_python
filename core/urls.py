ac8357adebadc906aac05a9dc3229881a2874b9f
dan <daniel.ma09@aluno.ifsc.edu.br>
2026-09-10 18:01:10 -0300

    graficos 1 e 2

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = {
     path('', views.index, name='urlindex'),
+    path('analise01/', views.line_chart_view, name="urlanalise01"),
+    path('analise02/', views.bar_chart_view, name="urlanalise02"),
+    path('analise03/', views.line_chart_view, name="urlanalise03"),
+    path('analise04/', views.line_chart_view, name="urlanalise04"),
+    path('analise05/', views.line_chart_view, name="urlanalise05"),
 }
--- core/views.py
+++ core/views.py
@@ -3,9 +3,12 @@ from urllib import request
 import io
 import urllib, base64
 import pandas as pd
-import matplotlib as plt
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
 from .models import Tweet
+from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
@@ -25,7 +28,7 @@ def plot_to_base64(fig):
     plt.close(fig)
     return img_b64
 
-def line_chart_view():
+def line_chart_view(request):
     # Evolução do número de tweets por palavra-chave (top 15)
     df = get_dataframe()
     top = df['keyword'].value_counts().head(15).sort_values()
@@ -34,9 +37,27 @@ def line_chart_view():
     ax.set_title('Top 15 Keywords — Número de Tweets (Linhas)')
     ax.set_xlabel('Keyword')
     ax.set_ylabel('Quantidade')
+    plt.grid(alpha=0.5)
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     grafico_linhas = plot_to_base64(plt.gcf())
-    plt.close()
+    plt.close(fig)
     context = {'grafico_linhas': grafico_linhas,}
-    return render(request, 'core/index.html', context)
+    return render(request, 'analise01.html', context)
+
+def bar_chart_view(request):
+    # Distribuição dos Tweets por Classe
+    df = get_dataframe()
+    qtde_tweets = df['target'].value_counts().sort_index()
+    labels = ['Não Desastre', 'Desastre']
+    fig, ax = plt.subplots(figsize=(9, 5))
+    ax.bar(labels, qtde_tweets.values, color=['skyblue','coral'])
+    ax.set_title('Distribuição de Tweets por Classe (Barras)')
+    ax.set_ylabel('Quantidade')
+    plt.grid(alpha=0.2)
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    grafico_barras = plot_to_base64(plt.gcf())
+    plt.close(fig)
+    context = {'grafico_barras': grafico_barras,}
+    return render(request, 'analise02.html', context)
